6756cdf5201f5edb12506b5fbb30ef28c14e8e45
GustavoLCR <gugulcr@gmail.com>
2019-09-22 01:50:51 -0300

    Add Cutter 1.9.0

---
+++ cutter/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop';
+$installDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" | Split-Path -Parent
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $installDir
+  softwareName  = 'cutter*'
+  url64bit      = 'https://github.com/radareorg/cutter/releases/download/v1.9.0/Cutter-v1.9.0-x64.Windows.zip'
+  checksum64    = '5dc2d2199f0ef2cc2483fc4f6dc4af6e7a3f0d410bc5aeaeba8b0ad2403da6a0'
+  checksumType64= 'sha256'
+}
+
+New-Item "$installDir\\cutter.gui" -type file -force | Out-Null
+New-Item "$installDir\\vc_redist.x64.exe.ignore" -type file -force | Out-Null
+
+Install-ChocolateyZipPackage @packageArgs
+
+$shortcutArgs = @{
+  Description = "Open Cutter"
+  TargetPath  = "$installDir\\cutter.exe"
+  WorkingDirectory = $installDir
+}
+$shortcutName = '\Cutter.lnk'
+
+$doCreateShortcut = Read-Host "Do you want to create a shortcut in the Desktop? (y/N)"
+if ($doCreateShortcut.Contains('y')) {
+  Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath  $([Environment]::GetFolderPath("Desktop") + "$shortcutName")
+}
+
+$doCreateShortcut = Read-Host "Do you want to create a shortcut in the Start Menu? (y/N)"
+if ($doCreateShortcut.Contains('y')) {
+  Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath $([Environment]::GetFolderPath("StartMenu") + $shortcutName)
+}
---
+++ cutter/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$doDeleteCfg = Read-Host "Do you wish to delete your configuration profile? (y/N)"
+if ($doDeleteCfg.Contains("y")) {
+	Remove-Item -Path "$env:APPDATA\\RadareOrg\\*cutter*" -Recurse -ErrorAction SilentlyContinue
+}
+
+$shortcutName = "\Cutter.lnk"
+
+Remove-Item -Path $([Environment]::GetFolderPath("Desktop") + $shortcutName) -ErrorAction SilentlyContinue
+Remove-Item -Path $([Environment]::GetFolderPath("StartMenu") + $shortcutName) -ErrorAction SilentlyContinue
