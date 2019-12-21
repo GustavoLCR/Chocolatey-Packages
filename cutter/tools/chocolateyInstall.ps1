8a083751f5ac0060307537284e020f7d541c1803
GustavoLCR <gugulcr@gmail.com>
2019-12-20 21:24:41 -0300

    Release Cutter 1.10.0

--- cutter/tools/chocolateyInstall.ps1
+++ cutter/tools/chocolateyInstall.ps1
@@ -1,19 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 $installDir = $env:ChocolateyPackageFolder
 
+$zipFile = "$installDir\\Cutter-v1.10.0-x64.Windows.zip"
+
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $installDir
-  softwareName  = 'cutter*'
-  url64bit      = 'https://github.com/radareorg/cutter/releases/download/v1.9.0/Cutter-v1.9.0-x64.Windows.zip'
-  checksum64    = '5dc2d2199f0ef2cc2483fc4f6dc4af6e7a3f0d410bc5aeaeba8b0ad2403da6a0'
-  checksumType64= 'sha256'
+  PackageName    = $env:ChocolateyPackageName
+  Destination    = $installDir
+  FileFullPath64 = $zipFile
 }
 
-New-Item "$installDir\\cutter.gui" -type file -force | Out-Null
+New-Item "$installDir\\cutter.exe.gui" -type file -force | Out-Null
 New-Item "$installDir\\vc_redist.x64.exe.ignore" -type file -force | Out-Null
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
 
 $shortcutArgs = @{
   Description = "Open Cutter"
@@ -31,3 +30,5 @@ $doCreateShortcut = Read-Host "Do you want to create a shortcut in the Start Men
 if (!$doCreateShortcut -or $doCreateShortcut.Contains('y')) {
   Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath $([Environment]::GetFolderPath("StartMenu") + $shortcutName)
 }
+
+Remove-Item -Path $zipFile -ErrorAction SilentlyContinue
