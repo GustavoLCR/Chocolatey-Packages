c55d19b010cd0a3f805111f7fb1db89901e4f818
GustavoLCR <gugulcr@gmail.com>
2021-07-30 20:04:51 -0300

    Rizin 0.2.1

--- rizin/tools/chocolateyinstall.ps1
+++ rizin/tools/chocolateyinstall.ps1
@@ -1,13 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url64 = 'https://github.com/rizinorg/rizin/releases/download/v0.1.0/rizin-windows-static-v0.1.0.zip'
+$url = 'https://github.com/rizinorg/rizin/releases/download/v0.2.1/rizin_installer-v0.2.1-x86.exe'
+$url64 = 'https://github.com/rizinorg/rizin/releases/download/v0.2.1/rizin_installer-v0.2.1-x86_64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $env:ChocolateyPackageFolder
+  filetype       = 'exe'
+  silentArgs     = '/VERYSILENT /CURRENTUSER /NORESTART'
+  url            = $url
   url64bit       = $url64
-  checksum64     = 'D09A8DEE4C74BF26D4642F93CD263DC635238D355BE621F16D6554ACA5AA9021'
+  checksum       = '91B3A5954F2FB252EDB9FF06304DFE7F3DC4B0D35208A17A156C66CBB1D039F9'
+  checksumType   = 'sha256'
+  checksum64     = '6C4E75AD777E1B16B2C18BDC46184F3523B376A3C45316C211FAB91507A930A7'
   checksumType64 = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
