3ee08d5da57cc780188b0917bf0793e95217c2e9
GustavoLCR <gugulcr@gmail.com>
2021-09-25 20:06:11 -0300

    Cutter 2.0.3

--- cutter/tools/chocolateyInstall.ps1
+++ cutter/tools/chocolateyInstall.ps1
@@ -1,14 +1,16 @@
 $ErrorActionPreference = 'Stop';
-$installDir = $env:ChocolateyPackageFolder
 
-$zipFile = "$installDir\\Cutter-v1.12.0-x64.Windows.zip"
+$version = "v2.0.3"
+$packageName = "Cutter-$version-x64.Windows"
+$zipFile = "$packageName.zip"
+$installRootDir = $env:ChocolateyPackageFolder
+$installDir = "$installRootDir\\$packageName"
 
 $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
-  unzipLocation  = $installDir
-  softwareName   = 'cutter*'
-  url64bit       = 'https://github.com/rizinorg/cutter/releases/download/v2.0.2/Cutter-v2.0.2-x64.Windows.zip'
-  checksum64     = 'E2D7E8FDA40BFFE62C4F35E1E34694687C0BC31BC8620ADBE9BD7BEDE7B6AF25'
+  unzipLocation  = $installRootDir
+  url64bit       = "https://github.com/rizinorg/cutter/releases/download/$version/$zipFile"
+  checksum64     = 'CBF279E5E7F690FFC5F028E6BD631DB182E87FE19DBF5DF66AD15F0BB8431A5C'
   checksumType64 = 'sha256'
 }
 
@@ -20,13 +22,13 @@ Install-ChocolateyZipPackage @packageArgs
 $shortcutArgs = @{
   Description      = "Open Cutter"
   TargetPath       = "$installDir\\cutter.exe"
-  WorkingDirectory = $installDir
+  WorkingDirectory = "$installDir"
 }
-$shortcutName = "\\Cutter.lnk"
+$shortcutName = '\Cutter.lnk'
 
 $doCreateShortcut = Read-Host "Do you want to create a shortcut in the Desktop? (Y/n)"
 if (!$doCreateShortcut -or $doCreateShortcut.Contains('y')) {
-  Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath  $([Environment]::GetFolderPath("Desktop") + "$shortcutName")
+  Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath  $([Environment]::GetFolderPath("Desktop") + $shortcutName)
 }
 
 $doCreateShortcut = Read-Host "Do you want to create a shortcut in the Start Menu? (Y/n)"
@@ -34,4 +36,4 @@ if (!$doCreateShortcut -or $doCreateShortcut.Contains('y')) {
   Install-ChocolateyShortcut @shortcutArgs -ShortcutFilePath $([Environment]::GetFolderPath("StartMenu") + $shortcutName)
 }
 
-Remove-Item -Path $zipFile -ErrorAction SilentlyContinue
+Remove-Item -Path "$installRootDir\\$zipFile" -ErrorAction SilentlyContinue
--- cutter/tools/chocolateyUninstall.ps1
+++ cutter/tools/chocolateyUninstall.ps1
@@ -1,9 +1,9 @@
-$doDeleteCfg = Read-Host "Do you wish to delete your configuration profile? (Y/n)"
-if (!$doDeleteCfg -or $doDeleteCfg.Contains("y")) {
-	Remove-Item -Path "$env:APPDATA\\RizinOrg\\*cutter*" -Recurse -ErrorAction SilentlyContinue
+$doDeleteCfg = Read-Host "Do you wish to delete your configuration profile? (y/N)"
+if ($doDeleteCfg.Contains("y")) {
+	Remove-Item -Path "$env:APPDATA\\rizin\\cutter*" -Recurse -ErrorAction SilentlyContinue
 }
 
-$shortcutName = "\Cutter.lnk"
+$shortcutName = '\Cutter.lnk'
 
 Remove-Item -Path $([Environment]::GetFolderPath("Desktop") + $shortcutName) -ErrorAction SilentlyContinue
 Remove-Item -Path $([Environment]::GetFolderPath("StartMenu") + $shortcutName) -ErrorAction SilentlyContinue
