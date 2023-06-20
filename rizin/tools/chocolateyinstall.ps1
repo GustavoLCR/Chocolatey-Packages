13c6afecd2f9fdad8de5aac1eeb55dfc75d1e07f
GustavoLCR <gugulcr@gmail.com>
2023-06-20 01:32:52 -0300

    Rizin 0.5.2

--- rizin/tools/chocolateyinstall.ps1
+++ rizin/tools/chocolateyinstall.ps1
@@ -1,18 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = 'v0.4.1'
+$version = 'v0.5.2'
 $url = "https://github.com/rizinorg/rizin/releases/download/$version/rizin_installer-$version-x86.exe"
 $url64 = "https://github.com/rizinorg/rizin/releases/download/$version/rizin_installer-$version-x86_64.exe"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   filetype       = 'exe'
-  silentArgs     = '/VERYSILENT /NORESTART'
+  silentArgs     = '/SP- /VERYSILENT /NORESTART'
   url            = $url
   url64bit       = $url64
-  checksum       = '4476E42DE1DFBA64B70427C811F908E01509BE182491556790EFFDB48215B484'
+  checksum       = 'D24EE7618B190197C5B5D1156B838D1EA8F1CB08E458C97E466AA2A2534699C8'
   checksumType   = 'sha256'
-  checksum64     = 'C29E8E83FC235E3603F9731C13DCFE915971871871390C766E4F2505D31BB613'
+  checksum64     = '70C9E49C1E859E7588D7BD41889285E831E51679FAEB026D1A290D5372C74E62'
   checksumType64 = 'sha256'
 }
 
